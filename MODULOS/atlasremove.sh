94ad9ff739ee8b5014ff9cf0226510f7e8fe2175
2812454 <2812454@gmail.com>
2024-07-19 00:58:34 +0200

    Update atlasremove.sh

--- MODULOS/atlasremove.sh
+++ MODULOS/atlasremove.sh
@@ -2,25 +2,62 @@
 username=$1
 uuid=$2
 
-if cat /etc/passwd |grep -w $user > /dev/null; then
-echo ""
-pkill -f "$username" > /dev/null 2>&1
-deluser --force $username > /dev/null 2>&1
-echo -e "\E[41;1;37m Usuario $username removido com sucesso! \E[0m"
-grep -v ^$username[[:space:]] /root/usuarios.db > /tmp/ph ; cat /tmp/ph > /root/usuarios.db
-rm /etc/SSHPlus/senha/$username 1>/dev/null 2>/dev/null
 
-fi     
-[[ $(sed -n '/'${username}'/=' /etc/v2ray/config.json|head -1) ]] || invaliduuid
-lineP=$(sed -n '/'${username}'/=' /etc/v2ray/config.json)
-linePre=$(sed -n '/'${username}'/=' /etc/RegV2ray)
-sed -i "${linePre}d" /etc/SSHPlus/RegV2ray
-numl1=2
-let resta=$lineP-$numl1
-sed -i "${resta}d" /etc/v2ray/config.json
-sed -i "${resta}d" /etc/v2ray/config.json
-sed -i "${resta}d" /etc/v2ray/config.json
-sed -i "${resta}d" /etc/v2ray/config.json
-sed -i "${resta}d" /etc/v2ray/config.json
+
+invalid_username() {
+        echo "USERNAME invÃƒÂ¡lido"
+        exit 1
+    }
+
+_getUser() {
+    user_count=$(grep "^$1:x:" /etc/passwd | cut -d ':' -f 1 | wc -c)
+    echo $user_count | tr -dc '0-9'
+}
+
+if [ -z "${username}" ]; then
+    echo "VocÃƒÂª deve especificar um usuÃƒÂ¡rio."
+    exit 1
+else
+    user_count=$(_getUser $username)
+    uuid_count=$(_getUser $uuid)
+    if [ "$user_count" -gt 3 ]; then
+        kill -9 $(ps -fu $username | awk '{print $2}' | grep -v PID)
+        userdel $username
+        echo "1"
+        grep -v "^$username[[:space:]]" /root/usuarios.db > /tmp/ph
+        cat /tmp/ph > /root/usuarios.db
+        rm /etc/SSHPlus/senha/$username 1>/dev/null 2>/dev/null
+        rm /etc/usuarios/$username 1>/dev/null 2>/dev/null
+        
+        grep -v "^$username[[:space:]]" /etc/SSHPlus/RegV2ray > /tmp/ph
+        cat /tmp/ph > /etc/SSHPlus/RegV2ray
+        
+    else
+        invalid_username
+    fi
+fi
+
+delete_uuid() {
+    if [ "$#" -ne 2 ]; then
+        echo "Uso: $0 <uuid> <login>"
+        exit 1
+    fi
+
+
+    invalid_uuid() {
+        echo "UUID invÃƒÂ¡lido"
+        exit 1
+    }
+
+    if grep -q "$uuid" /etc/v2ray/config.json; then
+        tmpfile=$(mktemp)
+        jq --arg uuid "$uuid" 'del(.inbounds[0].settings.clients[] | select(.id == $uuid))' /etc/v2ray/config.json > "$tmpfile" && mv "$tmpfile" /etc/v2ray/config.json
+
+        sudo systemctl restart v2ray
+        echo "Objeto com 'id' igual a $uuid removido"
+    else
+        invalid_uuid
+    fi
+}
 echo "  $username | $uuid  " >> /root/atlasremove.log
-v2ray restart > /dev/null 2>&1
+delete_uuid "$username" "$uuid"
